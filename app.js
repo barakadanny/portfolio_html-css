f0e4ae0c67c48a8d382c200a44ca2988d74ebced
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-07-21 16:10:43 +0300

    Work modal added

--- app.js
+++ app.js
@@ -1,6 +1,95 @@
 const navToggleClose = document.querySelector('.nav-toggle-c');
 const menu = document.querySelector('.nav-menu');
 const navToggleOpen = document.querySelector('.nav-toggle');
+const worksCard = document.querySelector('.works');
+const modalCard = document.querySelector('.modal');
+
+// modal selectors
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlays');
+
+// work data object
+const works = [
+  {
+    id: 'modal1',
+    title: 'Tonic',
+    description: 'This is a portfolio website for my personal portfolio.',
+    image: 'img/work-1.png',
+    chronical: ['CANOPY', 'Back End Dev', '2015'],
+    stack: ['html', 'css', 'javascript'],
+    liveurl: 'https://github.com/barakadanny',
+    githuburl: 'https://github.com/barakadanny',
+  },
+  {
+    id: 'modal2',
+    title: 'Multi-Post stories',
+    description: 'This is a portfolio website for my personal portfolio.',
+    image: 'img/work-2.png',
+    chronical: ['CANOPY', 'Back End Dev', '2015'],
+    stack: ['html', 'css', 'javascript'],
+    liveurl: 'https://github.com/barakadanny',
+    githuburl: 'https://github.com/barakadanny',
+  },
+  {
+    id: 'modal3',
+    title: 'Tonic',
+    description: 'This is a portfolio website for my personal portfolio.',
+    image: 'img/work-3.png',
+    chronical: ['CANOPY', 'Back End Dev', '2015'],
+    stack: ['html', 'css', 'javascript'],
+    liveurl: 'https://github.com/barakadanny',
+    githuburl: 'https://github.com/barakadanny',
+  },
+  {
+    id: 'modal4',
+    title: 'Multi-Post stories',
+    description: 'This is a portfolio website for my personal portfolio.',
+    image: 'img/work-4.png',
+    chronical: ['CANOPY', 'Back End Dev', '2015'],
+    stack: ['html', 'css', 'javascript'],
+    liveurl: 'https://github.com/barakadanny',
+    githuburl: 'https://github.com/barakadanny',
+  },
+];
+
+// generate dynamic work Cards
+for (var i = 0; i < works.length; i++) {
+  const work = works[i];
+
+  const workElement = document.createElement('div');
+  workElement.classList.add('work');
+  workElement.innerHTML = `
+    <div class="single-work">
+      
+      <div class="work-img">
+        <img src="${work.image}" alt="${work.title}">
+        </div>
+        <div class="work-content">
+          <h2 class="work-title">${work.title}</h2>
+          <ul class="work-chronical">
+            <li class="work-chronical-title">${work.chronical[0]}</li>
+            <li class="work-chronical_c">${work.chronical[1]}</li>
+            <li class="work-chronical_c">${work.chronical[2]}</li>
+          </ul>
+          <p class="work-description">${work.description}</p>
+          <ul class="work-stacks">
+            <li class="single-stack">${work.stack[0]}</li>
+            <li class="single-stack">${work.stack[1]}</li>
+            <li class="single-stack">${work.stack[2]}</li>
+          </ul>
+          <button data-modal-target="#modal" data-id="${work.id}" class="primary-btn">See Projects</button>
+        </div>
+      </div>
+    </div>
+  `;
+  worksCard.appendChild(workElement);
+  const modal = document.querySelector(`#${work.id}`);
+  workElement.addEventListener('click', () => {
+    dynamicModal(modal);
+  });
+}
+console.log('id ${work.id}');
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
 
 // Event listener open the menu
 navToggleOpen.addEventListener('click', (e) => {
@@ -13,3 +102,48 @@ navToggleClose.addEventListener('click', (e) => {
   e.preventDefault();
   menu.classList.remove('show-menu');
 });
+
+// generate dynamic modal cards
+
+openModalButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    const modal = document.querySelector(e.target.dataset.modalTarget);
+    dynamicModal(modal);
+  });
+});
+
+openModalButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.actives');
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
+
+closeModalButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    const modal = button.closest('.modal');
+    closeModal(modal);
+  });
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add('actives');
+  overlay.classList.add('actives');
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove('actives');
+  overlay.classList.remove('actives');
+}
