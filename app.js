677d77c2f24cc26f0fd54d2b2d62253f5ec4d651
Elijah Odjegba <elijahodjegba@gmail.com>
2022-07-22 09:26:26 +0100

    Form Validation

--- app.js
+++ app.js
@@ -151,3 +151,20 @@ closeModalButtons.forEach((button) => {
     closeModal(modal);
   });
 });
+
+/* Form Validation */
+
+const email = document.getElementById('email');
+const form = document.getElementById('input');
+const displayMsg = document.getElementById('form-message-error');
+
+form.addEventListener('submit', (event) => {
+  if (email.value !== email.value.toLowerCase()) {
+    event.preventDefault();
+    displayMsg.style.visibility = 'visible';
+    displayMsg.classList.add('error-message');
+    displayMsg.textContent = 'Your email address should be in lowercase';
+  } else {
+    displayMsg.style.visibility = 'hidden';
+  }
+});
